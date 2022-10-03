29f51fb76358b91051c84dd199911c916a9af54f
Badreddine-Ab <badreddine@gmail.com>
2022-10-03 15:38:12 +0100

    structer update + article backend completed

---
+++ models/articleModel.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize,DataTypes) => {
+    const Article = sequelize.define("articles", {
+        title: {
+            type: DataTypes.STRING,
+            // allowNull: false
+        },
+        content:{
+            type: DataTypes.STRING,
+            // allowNull: false
+        },
+        images:{
+            type:DataTypes.STRING,
+        },
+        description:{
+            type:DataTypes.TEXT
+        },
+        published:{
+            type:DataTypes.BOOLEAN
+        }
+       
+    })
+
+    return Article
+}
---
+++ models/index.js
@@ -0,0 +1,43 @@
+const dbConfig = require('../config/dbConfig.js');
+
+const Sequilize = require('sequelize')
+const DataTypes = Sequilize.DataTypes
+
+const sequelize = new Sequilize(
+    dbConfig.DB,
+    dbConfig.USER,
+    dbConfig.PASSWORD, {
+        host : dbConfig.HOST,
+        dialect: dbConfig.dialect,
+        operatorsAliases: false,
+
+        pool:{
+            max : dbConfig.pool.max,
+            min : dbConfig.pool.min,
+            acquire : dbConfig.pool.acquire,
+            idle: dbConfig.pool.idle
+        }
+    }
+)
+
+sequelize.authenticate()
+.then(()=> {
+    console.log('connected...')
+})
+.catch(err => {
+    console.log('error' + err)
+})
+
+const db = {}
+
+db.Sequelize = Sequilize
+db.sequelize = sequelize
+
+db.articles = require('./articleModel.js')(sequelize,DataTypes)
+
+db.sequelize.sync({force: false})
+.then(()=> {
+    console.log('yes re-sync done!')
+})
+
+module.exports = db
