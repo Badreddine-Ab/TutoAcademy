83e2e1a95421c45a67ab9f38bd980f0519a41338
chaimaa et-toumy <chaimaetoumy5@gmail.com>
2022-10-03 19:48:47 +0100

    adding all router that i well need in categorie Router

---
+++ routes/categorieRouter.js
@@ -0,0 +1,15 @@
+const productController = require('../controllers/categorieController.js')
+
+const routerCategorie = require('express').Router()
+
+routerCategorie.post('/addCategorie',productController.addCategorie)
+
+routerCategorie.get('/AllCategorie',productController.getAllCategorie)
+
+routerCategorie.get('/:id',productController.getOneCategorie)
+
+routerCategorie.put('/:id',productController.updateCategorie)
+
+routerCategorie.delete('/:id',productController.deleteCategorie)
+
+module.exports = routerCategorie
