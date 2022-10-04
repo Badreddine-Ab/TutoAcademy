24c81824eb76d2406456708a7e0cdf117f110398
chaimaa et-toumy <chaimaetoumy5@gmail.com>
2022-10-04 14:18:06 +0100

    add a router for each page in addition to linking all the pages and adding the public folder path to server.js so that the pages are displayed with css and images

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extend: true}))
 
+// set the view engine to ejs
+app.set('views','./view');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
 
 // routers
 const router = require('./routes/articleRouter.js')
@@ -19,9 +24,39 @@ app.use('/api/categories',routerCategorie)
 
 
 //testing api 
-app.get('/',(req,res) => {
-    res.json({message: 'hello from api'})
+app.get("/",(req,res) => {
+    res.render('index')
 })
+app.get("/about",(req,res) => {
+    res.render('about')
+})
+app.get("/category",(req,res) => {
+    res.render('category')
+})
+app.get("/single-video",(req,res) => {
+    res.render('single-video')
+})
+app.get("/single-audio",(req,res) => {
+    res.render('single-audio')
+})
+app.get("/single-gallery",(req,res) => {
+    res.render('single-gallery')
+})
+app.get("/single-standard",(req,res) => {
+    res.render('single-standard')
+})
+app.get("/style-guide",(req,res) => {
+    res.render('style-guide')
+})
+app.get("/contact",(req,res) => {
+    res.render('contact')
+})
+// app.get("/single-standard",(req,res) => {
+//     res.render('single-standard')
+// })
+// app.get("/single-standard",(req,res) => {
+//     res.render('single-standard')
+// })
 
 // port
 
